1c17b62c1f501ecb6bcdeb8e02ae2c163a0b2de1
Ivan Novikov <i.novikov@iconic.vc>
2019-03-01 23:04:33 +0300

    del conn_timeout

--- market_maker/ws/ws_thread.py
+++ market_maker/ws/ws_thread.py
@@ -171,7 +171,7 @@ class BitMEXWebsocket():
         conn_timeout = 5
         while (not self.ws.sock or not self.ws.sock.connected) and conn_timeout and not self._error:
             sleep(1)
-            conn_timeout -= 1
+            # conn_timeout -= 1
 
         if not conn_timeout or self._error:
             self.logger.error("Couldn't connect to WS! Exiting.")

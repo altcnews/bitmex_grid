6a59b14192e2e88e68d2ade780eaaf156c1e74bf
Ivan Novikov <i.novikov@iconic.vc>
2019-03-02 12:03:16 +0300

    logging in file

--- market_maker/utils/log.py
+++ market_maker/utils/log.py
@@ -14,9 +14,9 @@ def setup_custom_logger(name, log_level=settings.LOG_LEVEL):
     logger.setLevel(log_level)
     logger.addHandler(handler)
 
-    # date = datetime.today().strftime("%Y%m%d")
-    # handler = logging.FileHandler('{}.log'.format(date))
-    # handler.setFormatter(formatter)
-    # logger.addHandler(handler)
+    date = datetime.today().strftime("%Y%m%d")
+    file_handler = logging.FileHandler('{}.log'.format(date))
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
     return logger
